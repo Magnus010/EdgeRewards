eb9d9a808d041ee653f786375751c16fa60ac9de
Bueter <magnus010@bueters.com>
2020-12-08 10:55:11 -0700

    Updating edge script.

--- EdgeRewards.py
+++ EdgeRewards.py
@@ -20,25 +20,32 @@ def kill_edge():
 
 def sleepy_time():
     time_to_sleep = random.randrange(30, 600)
-    print("Sleeping for " + time_to_sleep + " seconds.")
+    print("Sleeping for " + str(time_to_sleep) + " seconds.")
     time.sleep(time_to_sleep)
 
 
 if __name__ == "__main__":
     dictionary = open("words.txt").read().splitlines()
     edge_string = "start shell:AppsFolder\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge"
+    url = "https://www.bing.com/search?q="
 
     num_of_searches = int(150 / 5)
 
     print("Running Edge Rewards.")
     for search in range(0, num_of_searches):
         sleepy_time()
-        print("Search #: " + search)
+        print("Search #: " + str(search))
         num_of_terms = random.randrange(1, 5)
         search_terms = random.sample(dictionary, num_of_terms)
-        search_string = edge_string
+        search_string = edge_string + " " + url
+        first = True
         for term in search_terms:
-            search_string += " " + term
+            if first:
+                first = False
+            else:
+                search_string += "+"
+            search_string += term
+
         os.popen(search_string)
 
     sleepy_time()
